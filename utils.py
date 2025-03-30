98875a88c73b31e2230b28c5f3723a3a4963435f
Raj-shriwastava <rajshriwastav2318@gmail.com>
2025-03-31 01:14:06 +0530

    removed keys so that we can deploy it in cloud using stramlit

--- utils.py
+++ utils.py
@@ -1,14 +1,18 @@
 # intelligent_resume_analyzer_simple/utils.py
 import os
 from dotenv import load_dotenv
+import streamlit as st
 
 def load_api_key(api_name):
-    """Loads the API key from environment variables."""
-    load_dotenv()
-    api_key = os.getenv(api_name)
-    if not api_key:
-        raise ValueError(f"{api_name} API key not found in environment variables.")
-    return api_key
+    """Loads the API key from Streamlit Secrets or environment variables."""
+    if "GEMINI_API_KEY" in st.secrets:
+        return st.secrets["GEMINI_API_KEY"]
+    else:
+        load_dotenv()
+        api_key = os.getenv(api_name)
+        if not api_key:
+            raise ValueError(f"{api_name} API key not found in environment variables or Streamlit Secrets.")
+        return api_key
 
 def clean_text(text):
     """Cleans and normalizes the input text."""
